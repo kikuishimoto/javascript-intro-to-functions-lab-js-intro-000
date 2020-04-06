40f837ea51c20c6e81de14e5df6c1243118f9216
kiku ishimoto <kikuish@gmail.com>
2020-04-06 06:52:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,17 @@ function logWhisper(string) {
   var answerr = string.toLowerCase()
   console.log(answerr)
 }
+funtion sayHiToGrandma(string) {
+ var uppercase = string
+ var lowercase = string
+
+ if (uppercase.toUperCase() === uppercase) {
+   return `I can't hear you!`;
+ } else if (lowercase.toLowerCase()=== lowercase) {
+   return `YES INDEED!`;
+ } else if (string = "I love you, Grandma") {
+   return `I love you, too.`;
+ } else {
+   return `WHAT`;
+ }
+}
